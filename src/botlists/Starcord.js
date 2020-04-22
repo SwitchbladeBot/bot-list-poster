c4f0421729012d38e85469ec462238c154c8a106
pedrofracassi <pedroluizfracassi@gmail.com>
2020-04-22 14:52:58 +0000

    Add Statcord support

---
+++ src/botlists/Starcord.js
@@ -0,0 +1,27 @@
+const { BotList } = require('../structures')
+const fetch = require('node-fetch')
+
+module.exports = class Statcord extends BotList {
+  constructor (name) {
+    super({
+      name: 'statcord',
+      url: 'https://statcord.com/',
+      interval: 60
+    })
+  }
+
+  postStatistics ({ guildCount, botId, token }) {
+    fetch('https://statcord.com/apollo/post/stats', {
+      method: 'post',
+      body: JSON.stringify([
+        {
+          id: botId,
+          key: token,
+          servers: guildCount,
+          users: userCount
+        }
+      ])
+    })
+    return true
+  }
+}
