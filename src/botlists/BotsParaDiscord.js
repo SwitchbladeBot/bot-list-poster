c2d83c21ee98c8b2b94b2c0748c2ce80d2cdcdc9
Arthur Costa <63468042+gyinx@users.noreply.github.com>
2020-04-22 16:15:27 -0300

    Push

---
+++ src/botlists/BotsParaDiscord.js
@@ -0,0 +1,22 @@
+const { BotList } = require('../structures')
+const fetch = require('node-fetch')
+
+module.exports = class BotsParaDiscord extends BotList {
+  constructor (name) {
+    super({
+      name: 'botsparadiscord',
+      url: 'https://botsparadiscord.com/',
+      interval: 60
+    })
+  }
+
+  postStatistics ({ botId, guildCount, token }) {
+    fetch(`https://api.botsparadiscord.xyz/bots/${botId}/info?guilds=${guildCount}`, {
+      method: 'post',
+      headers: {
+          Authorization: token
+      }
+    })
+    return true
+  }
+}
