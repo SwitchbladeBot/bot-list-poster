5b07b5c4dcae9ab0f94da5acb1b49618c8cc1224
pedrofracassi <pedroluizfracassi@gmail.com>
2020-04-27 18:43:40 +0000

    Add Doc link and remove old URL

--- src/botlists/ArcaneCenter.js
+++ src/botlists/ArcaneCenter.js
@@ -1,11 +1,13 @@
 const { BotList } = require('../structures')
 const fetch = require('node-fetch')
 
+/**
+ * @see https://arcane-center.xyz/documentation
+ */
 module.exports = class ArcaneCenter extends BotList {
   constructor (name) {
     super({
       name: 'arcanecenter',
-      url: 'https://arcane-center.xyz/',
       interval: 60
     })
   }
